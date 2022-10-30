ba74eeeee4b2d4e39ca2872259af42af675ad4b7
Philemon Eniola <dev.mcenny@gmail.com>
2022-10-30 23:45:05 +0100

    ejs

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000)
+
+app.get('/', (req, res)=>{
+    // res.sendFile('./views/index.html', {root: __dirname}) 
+    res.render('index')
+})
+
+app.get('/about', (req, res)=>{
+    // res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about')
+})
+
+app.get('/about-me', (req, res)=>{
+    res.redirect('/about')
+})
+
+app.get(/create)
+
+app.use((req, res) => {
+    res.status(404).render('404')
+})
