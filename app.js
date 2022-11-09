cad35db1375d8046ef53e1fc3dc61773942a5dc4
Philemon Eniola <dev.mcenny@gmail.com>
2022-11-09 16:16:31 +0100

    MVC update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const { render } = require('ejs')
+const blogRoutes = require('./routes/blogRoutes')
 
 
 const app = express()
@@ -17,25 +18,12 @@ app.set('view engine', 'ejs')
 
 // middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'New Blog 2',
-        snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
-        body: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
-    });
-
-    blog.save()
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((error) => console.log('some error in saving the blog post'))
-})
 
 app.get('/all-blogs', (req, res) => {
-    Blog.find()
+    Blog.find().sort({ createdAt: -1 })
     .then((result) => {
         res.send(result)
     })
@@ -43,6 +31,8 @@ app.get('/all-blogs', (req, res) => {
         console.log('some error in fetching all blogs')
     })
 });
+
+app.use('/blogs', blogRoutes)
 
 app.get('/single-blog', (req, res) => {
     Blog.findById('63616259ea24195d9f8f7da0')
@@ -56,17 +46,14 @@ app.get('/single-blog', (req, res) => {
 
 // routes
 app.get('/', (req, res)=>{
-    // res.sendFile('./views/index.html', {root: __dirname}) 
-    const blogs = [
-        {title: 'Lorem ipsum dolor sit amet', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'},
-        {title: 'Lui minim labore adipisicing minim', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'},
-        {title: 'Ladipisicing minim sint cillum sint', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'}
-    ]
-    res.render('index', {title: 'Home', blogs})
-})
-
-app.get('/blogs', (req, res) => {
-    Blog.find()
+    // // res.sendFile('./views/index.html', {root: __dirname}) 
+    // const blogs = [
+    //     {title: 'Lorem ipsum dolor sit amet', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'},
+    //     {title: 'Lui minim labore adipisicing minim', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'},
+    //     {title: 'Ladipisicing minim sint cillum sint', snippet: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'}
+    // ]
+    // res.render('index', {title: 'Home', blogs})
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res)=>{
@@ -76,10 +63,6 @@ app.get('/about', (req, res)=>{
 
 app.get('/about-me', (req, res)=>{
     res.redirect('/about')
-})
-
-app.get('/blogs/create', (req, res)=>{
-    res.render('create', {title: 'Create new blog post'})
 })
 
 app.use((req, res) => {
---
+++ controllers/blogController.js
@@ -0,0 +1,58 @@
+const Blog = require('../models/blog')
+// blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
+
+const blog_index = (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((error) => {
+        console.log('there was an error fetching the data from the database')
+    })
+}
+
+const blog_details = (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+    .then((result) => {
+        res.render('details', {blog: result, title: result.title})
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+const blog_create_get = (req, res) => {
+    res.render('create', {title: 'Create a new Blog'})
+}
+
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then((result) => {
+        res.json({redirect: '/blogs'})
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
